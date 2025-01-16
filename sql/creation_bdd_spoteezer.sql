e4df4d223ad9c0828baf4adf1179b6c3b4c791cd
Thomas <thomasfouquet8@gmail.com>
2025-01-16 09:41:20 +0000

    Update mcd,mld and sql

--- sql/creation_bdd_spoteezer.sql
+++ sql/creation_bdd_spoteezer.sql
@@ -4,13 +4,11 @@ use spoteezer;
 -- Création des tables
 
 /*
-CREATE TABLE Artist(
-   id_artist INT AUTO_INCREMENT,
-   firstname_artist VARCHAR(50) ,
-   lastname_artist VARCHAR(50) ,
-   alias_artist VARCHAR(50) ,
-   description_artist VARCHAR(500) ,
-   PRIMARY KEY(id_artist)
+CREATE TABLE Album(
+   id_album INT AUTO_INCREMENT,
+   name_album VARCHAR(50) ,
+   publication_date_album DATE NOT NULL,
+   PRIMARY KEY(id_album)
 );
 
 CREATE TABLE User_type(
@@ -25,29 +23,39 @@ CREATE TABLE Music_Genre(
    PRIMARY KEY(id_genre)
 );
 
+CREATE TABLE Type_Artist(
+   id_type_artist INT AUTO_INCREMENT,
+   libelle_type_artist VARCHAR(50)  NOT NULL,
+   PRIMARY KEY(id_type_artist)
+);
+
+CREATE TABLE Title(
+   id_title INT AUTO_INCREMENT,
+   name_title VARCHAR(50)  NOT NULL,
+   time_title VARCHAR(50)  NOT NULL,
+   publication_date_title DATE NOT NULL,
+   id_genre INT NOT NULL,
+   PRIMARY KEY(id_title),
+   FOREIGN KEY(id_genre) REFERENCES Music_Genre(id_genre)
+);
+
+CREATE TABLE Artist(
+   id_artist INT AUTO_INCREMENT,
+   firstname_artist VARCHAR(50) ,
+   lastname_artist VARCHAR(50) ,
+   alias_artist VARCHAR(50) ,
+   description_artist VARCHAR(500) ,
+   id_type_artist INT NOT NULL,
+   PRIMARY KEY(id_artist),
+   FOREIGN KEY(id_type_artist) REFERENCES Type_Artist(id_type_artist)
+);
+
 CREATE TABLE Playlist(
    id_playlist INT AUTO_INCREMENT,
    name_playlist VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(id_playlist)
-);
-
-CREATE TABLE Groupe(
-   id_groupe INT AUTO_INCREMENT,
-   name_groupe VARCHAR(500)  NOT NULL,
-   PRIMARY KEY(id_groupe)
-);
-
-CREATE TABLE Album(
-   id_album INT AUTO_INCREMENT,
-   name_album VARCHAR(50) ,
-   publication_date_album DATE NOT NULL,
-   id_groupe INT,
-   id_genre INT,
-   id_artist INT,
-   PRIMARY KEY(id_album),
-   FOREIGN KEY(id_groupe) REFERENCES Groupe(id_groupe),
-   FOREIGN KEY(id_genre) REFERENCES Music_Genre(id_genre),
-   FOREIGN KEY(id_artist) REFERENCES Artist(id_artist)
+   id_title INT,
+   PRIMARY KEY(id_playlist),
+   FOREIGN KEY(id_title) REFERENCES Title(id_title)
 );
 
 CREATE TABLE Users(
@@ -63,27 +71,13 @@ CREATE TABLE Users(
    FOREIGN KEY(id_type_user) REFERENCES User_type(id_type_user)
 );
 
-CREATE TABLE Title(
-   id_title INT AUTO_INCREMENT,
-   name_title VARCHAR(50)  NOT NULL,
-   time_title VARCHAR(50)  NOT NULL,
-   publication_date_title DATE NOT NULL,
-   id_playlist INT,
+CREATE TABLE Production(
+   id_title INT,
    id_album INT,
-   id_genre INT,
    id_artist INT,
-   PRIMARY KEY(id_title),
-   FOREIGN KEY(id_playlist) REFERENCES Playlist(id_playlist),
+   PRIMARY KEY(id_title, id_album, id_artist),
+   FOREIGN KEY(id_title) REFERENCES Title(id_title),
    FOREIGN KEY(id_album) REFERENCES Album(id_album),
-   FOREIGN KEY(id_genre) REFERENCES Music_Genre(id_genre),
    FOREIGN KEY(id_artist) REFERENCES Artist(id_artist)
-);
-
-CREATE TABLE Artist_Group(
-   id_artist INT,
-   id_groupe INT,
-   PRIMARY KEY(id_artist, id_groupe),
-   FOREIGN KEY(id_artist) REFERENCES Artist(id_artist),
-   FOREIGN KEY(id_groupe) REFERENCES Groupe(id_groupe)
 );
 */
