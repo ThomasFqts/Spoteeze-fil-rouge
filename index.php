1fc6664ef3947aef4d062e7fae61ee4088840202
helene <darte.helene@gmail.com>
2025-01-20 16:54:55 +0100

    nouvelle page cree + bouton fonctionnelle

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!---------------------------------- favicon ----------------------------->
-    <link rel="icon" type="image/png" sizes="32x32" href="Style/note-musicale.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/Style/img/icon.png">
     <!---------------------------------- titre ------------------------------->
     <title>Spoteezer</title>
     <!------------------------------ liens CSS ------------------------------->
@@ -13,14 +13,14 @@
 
 </head>
 
-<body> <!--  Tomas est agressif -->
+<body>
     <header id="enTete">
 
         <article id="logo">
             <img src="Style/img/logo.png" alt="Spoteezer" width="450" height="350">
         </article>
 
-        <!-- BOUTONS Y SONT LA !!!!!!!!!!!!!! ousa ? Jsplus -->
+        <!-------------------------------------------- Boutons Header ----------------------------------------------->
 
         <article id="boutons">
             <button id="seCo" style="width: 150px; height: 30px;">Se Connecter</button>
@@ -32,19 +32,45 @@
         </article>
 
     </header>
+
+    <!-------------------------------------------- Boutons Main ----------------------------------------------------->
+
     <main id="partiePrincipale">
 
         <article>
-            <bouton>
-                <img src="Style/img/pop.png" sizes width="150" height="70" id="boutonMusiquePop">
-            </bouton>
-            <bouton><img src="Style/img/PNG.png" sizes width="150" height="70" id="boutonMusiqueRock"></bouton>
-            <bouton><img src="Style/img/RAP.png" sizes width="150" height="70" id="boutonMusiqueRap"></bouton>
-            <bouton><img src="Style/img/WORKOUT.png" sizes width="150" height="70" id="boutonMusiqueWorkout"></bouton>
-            <bouton><img src="Style/img/RAP US.png" sizes width="150" height="70" id="boutonMusiqueRapUs"></bouton>
-            <bouton><img src="Style/img/dance.png" sizes width="150" height="70" id="boutonMusiqueDance"></bouton>
-            <bouton><img src="Style/img/funk.png" sizes width="150" height="70" id="boutonMusiqueFunk"></bouton>
-            <bouton><img src="Style/img/LOFI.png" sizes width="150" height="70" id="boutonMusiqueLofi"></bouton>
+
+            <a href="pop">
+                <img src="Style/img/pop.png" width="150" height="70" alt="Musique Pop">
+                link
+            </a>
+
+            <a href="Rock">
+                <img src="Style/img/PNG.png" sizes width="150" height="70" alt="Musique Rock">
+            </a>
+
+            <a href="Rap">
+                <img src="Style/img/RAP.png" sizes width="150" height="70" alt="Musique Rap">
+            </a>
+
+            <a href="Workout">
+                <img src="Style/img/WORKOUT.png" sizes width="150" height="70" alt="Musique Workout">
+            </a>
+
+            <a href="RapUS">
+                <img src="Style/img/RAP US.png" sizes width="150" height="70" alt="Musique RapUs">
+            </a>
+
+            <a href="Dance">
+                <img src="Style/img/dance.png" sizes width="150" height="70" alt="Musique Dance">
+            </a>
+
+            <a href="Funk">
+                <img src="Style/img/funk.png" sizes width="150" height="70" alt="Musique Funk">
+            </a>
+
+            <a href="Lofi">
+                <img src="Style/img/LOFI.png" sizes width="150" height="70" alt="Musique Lofi">
+            </a>
         </article>
 
     </main>
