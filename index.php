7306e483e48c554f766803fa4f381ea007a680cc
helene <darte.helene@gmail.com>
2025-01-21 12:52:14 +0100

    creation header

--- index.php
+++ index.php
@@ -1,91 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include "header.php"
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!---------------------------------- favicon ----------------------------->
-    <link rel="icon" type="image/png" sizes="32x32" href="/Style/img/icon.png">
-    <!---------------------------------- titre ------------------------------->
-    <title>Spoteezer</title>
-    <!------------------------------ liens CSS ------------------------------->
-    <link rel="stylesheet" href="Style\style.css">
+<!-------------------------------------------- Boutons Main ----------------------------------------------------->
 
-</head>
+<main id="partiePrincipale">
 
-<body>
-    <header id="enTete">
+    <article>
 
-        <article id="logo">
-            <img src="Style/img/logo.png" alt="Spoteezer" width="450" height="350">
-        </article>
+        <a href="pop">
+            <img src="Style/img/pop.png" width="150" height="70" alt="Musique Pop">
+        </a>
 
-        <!-------------------------------------------- Boutons Header ----------------------------------------------->
+        <a href="Rock">
+            <img src="Style/img/PNG.png" sizes width="150" height="70" alt="Musique Rock">
+        </a>
 
-        <article id="boutons">
-            <button id="seCo" style="width: 150px; height: 30px;">Se Connecter</button>
-            <button id="taPlaylist" style="width: 150px; height: 30px;">Ta Playlist</button>
-        </article>
+        <a href="Rap">
+            <img src="Style/img/RAP.png" sizes width="150" height="70" alt="Musique Rap">
+        </a>
 
-        <article id="recherche">
-            <input id="barreDeRecherche" type="text" placeholder="Rechercher..." style="width: 150px; height: 30px;">
-        </article>
+        <a href="Workout">
+            <img src="Style/img/WORKOUT.png" sizes width="150" height="70" alt="Musique Workout">
+        </a>
 
-    </header>
+        <a href="RapUS">
+            <img src="Style/img/RAP US.png" sizes width="150" height="70" alt="Musique RapUs">
+        </a>
 
-    <!-------------------------------------------- Boutons Main ----------------------------------------------------->
+        <a href="Dance">
+            <img src="Style/img/dance.png" sizes width="150" height="70" alt="Musique Dance">
+        </a>
 
-    <main id="partiePrincipale">
+        <a href="Funk">
+            <img src="Style/img/funk.png" sizes width="150" height="70" alt="Musique Funk">
+        </a>
 
-        <article>
+        <a href="Lofi">
+            <img src="Style/img/LOFI.png" sizes width="150" height="70" alt="Musique Lofi">
+        </a>
+    </article>
 
-            <a href="pop">
-                <img src="Style/img/pop.png" width="150" height="70" alt="Musique Pop">
-                link
-            </a>
+</main>
 
-            <a href="Rock">
-                <img src="Style/img/PNG.png" sizes width="150" height="70" alt="Musique Rock">
-            </a>
+<footer>
 
-            <a href="Rap">
-                <img src="Style/img/RAP.png" sizes width="150" height="70" alt="Musique Rap">
-            </a>
+    <article>
+    </article>
 
-            <a href="Workout">
-                <img src="Style/img/WORKOUT.png" sizes width="150" height="70" alt="Musique Workout">
-            </a>
+    <article>
+    </article>
 
-            <a href="RapUS">
-                <img src="Style/img/RAP US.png" sizes width="150" height="70" alt="Musique RapUs">
-            </a>
+</footer>
 
-            <a href="Dance">
-                <img src="Style/img/dance.png" sizes width="150" height="70" alt="Musique Dance">
-            </a>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="Script/scripts.js"></script>
 
-            <a href="Funk">
-                <img src="Style/img/funk.png" sizes width="150" height="70" alt="Musique Funk">
-            </a>
-
-            <a href="Lofi">
-                <img src="Style/img/LOFI.png" sizes width="150" height="70" alt="Musique Lofi">
-            </a>
-        </article>
-
-    </main>
-
-    <footer>
-
-        <article>
-        </article>
-
-        <article>
-        </article>
-
-    </footer>
-
-    <script src="Script/scripts.js"></script>
 </body>
 
 </html>
