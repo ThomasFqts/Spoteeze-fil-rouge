b866997e00c9bbdaa4f617fb538f25b7721389cb
Thomas <thomasfouquet8@gmail.com>
2025-01-29 11:52:09 +0100

    Avavncement de la recherche

--- search.php
+++ search.php
@@ -3,10 +3,25 @@ include "header.php";
 
 $db = ConnexionBase(); // Connexion à la base de données
 
+$search = isset($_GET['search']) ? $_GET['search'] : "";
+
 // Récupére les artistes, les titres et les albums
-$artists = $db->query("SELECT * FROM artist")->fetchAll(PDO::FETCH_ASSOC);
-$titles = $db->query("SELECT * FROM title")->fetchAll(PDO::FETCH_ASSOC);
-$albums = $db->query("SELECT * FROM album")->fetchAll(PDO::FETCH_ASSOC);
+$request = "SELECT * FROM production p 
+JOIN artist a ON p.id_artist = a.id_artist
+JOIN album al ON p.id_album = al.id_album
+JOIN title t ON p.id_title = t.id_title ";
+
+if (!empty($search)) {
+    $request .= "WHERE (name_album LIKE '%$search%') OR 
+    (name_title LIKE '%$search%') OR 
+    (firstname_artist LIKE '%$search%') OR 
+    (lastname_artist LIKE '%$search%') OR 
+    (alias_artist LIKE '%$search%') OR 
+    (firstname_artist AND lastname_artist LIKE '%$search%') OR
+    (lastname_artist AND firstname_artist LIKE '%$search%')";
+}
+
+$resultats = $db->query($request)->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -23,14 +38,25 @@ $albums = $db->query("SELECT * FROM album")->fetchAll(PDO::FETCH_ASSOC);
 
     <main>
 
-    <form action="search.php" method="GET">
-            <input id="barreDeRecherche" type="search" placeholder="Rechercher..."  class="form-control mr-sm-2">
+        <form action="search.php" method="GET" class="form-inline">
+            <input id="barreDeRecherche" type="search" name="search" placeholder="Rechercher..." class="form-control mr-sm-2">
             <button type="submit" class="btn btn-primary">Rechercher</button>
         </form>
-        
-        <label><input type="radio" name="type_entity" value="artist"> Artiste </label>
-        <label><input type="radio" name="type_entity" value="title"> Titre </label>
-        <label><input type="radio" name="type_entity" value="album"> Album </label>
+
+        <section>
+            <?php if (count($resultats) > 0): ?>
+                <?php foreach ($resultats as $resultat): ?>
+                    <div>
+                        <p><?= htmlentities($resultat['name_title']) ?></p>
+                        <p><?= htmlentities($resultat['time_title']) ?></p>
+                        <p><?= htmlentities($resultat['alias_artist']) ?></p>
+                    </div>
+                <?php endforeach ?>
+            <?php else: ?>
+                <p>Aucun artist, titre ou album trouvé</p>
+            <?php endif ?>
+        </section>
+
 
     </main>
 
